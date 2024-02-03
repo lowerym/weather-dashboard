039fe1424f479bb579499e629bb04e1c8548f32d
Matthew Lowery <matthewdlowery@msn.com>
2024-02-03 15:37:39 -0600

    Further built out main page and styles, and built basic city search function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,18 @@
+var searchBtn = document.getElementById("searchBtn")
 var APIkey = "aad29e9f536c08c7d583250641f40a35";
-var city;
-var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIkey;
 
-fetch(queryURL);
+  function getAPI(city){
+    fetch ("http://api.openweathermap.org/geo/1.0/direct?q=" + city + "&appid="+ APIkey)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+      });
+
+  }
+
+  searchBtn.addEventListener("click", function(){
+    var city = document.querySelector("#citySearch").value
+    getAPI(city);
+  })
